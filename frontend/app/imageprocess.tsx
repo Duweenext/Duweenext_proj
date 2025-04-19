7e84fb78dcae7ffb8c7eeb114fa9323592d2cf4d
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-04-20 00:54:03 +0700

    [Add] addition screen

---
+++ frontend/app/imageprocess.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ImageProcess = () => {
+    return (
+        <View>
+            <Text>ImageProcess</Text>
+        </View>
+    )
+}
+
+export default ImageProcess

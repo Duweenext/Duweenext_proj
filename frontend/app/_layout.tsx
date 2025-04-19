8c38a67b7f7d7565e03e36bea18b19611d023937
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-04-20 00:49:12 +0700

    [Setup] main navigation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,43 @@
-import { Stack } from "expo-router";
-import './globals.css';
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect, useState } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{
-        headerShown: false,
-      }}
-    />
-  </Stack>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await SecureStore.getItemAsync('token');
+      setIsAuthenticated(!!token);
+      setIsLoading(false);
+    };
+
+    checkToken();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const inAuthGroup = segments[0] === 'auth';
+
+      if (!isAuthenticated && !inAuthGroup) {
+        router.replace('/auth/login');
+      } else if (isAuthenticated && inAuthGroup) {
+        router.replace('/(tabs)');
+      }
+    }
+  }, [isLoading, isAuthenticated, segments]);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Slot />;
 }

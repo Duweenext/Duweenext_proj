05d1c3e9a2c3ca92fb2c79e0c4b7105cbdec639f
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-04-20 17:15:21 +0700

    [Update] style
    - change from native style to nativewind

--- frontend/app/auth/login.tsx
+++ frontend/app/auth/login.tsx
@@ -1,9 +1,8 @@
-import { View, Text, Image, TextInput, ImageBackground, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import React, { useState } from 'react'
 import { images } from '@/constants/images';
 import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
 import { useRouter } from 'expo-router';
-import style from './authstyle';
 import { icons } from '@/constants/icons';
 
 const Login = () => {
@@ -12,62 +11,82 @@ const Login = () => {
     const navigation = useRouter();
 
     return (
-        <View style={{ flex: 1 }}>
-            <Image source={images.background} style={{ height: "100%", position: 'absolute' }} className='h-full absolute' />
-            {/* <ImageBackground source={images.background} style={{ height: "100%", flex: 1 }}> */}
-            <View style={style.outerbox}>
-                <View style={style.innerbox} >
-                    {/* logo section */}
-                    <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} source={images.highlight} style={style.logo_section} />
+        <KeyboardAvoidingView
+            className='flex-1'
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <View className='flex-1'>
+                    <Image source={images.background} className='h-full absolute' />
+                    {/* <ImageBackground source={images.background} style={{ height: "100%", flex: 1 }}> */}
+                    <View className='flex-col h-full w-full justify-around pt-10 pb-10'>
+                        <View className='flex-col items-start gap-5 p-11' >
+                            {/* logo section */}
+                            <Animated.Image
+                                entering={FadeInUp.delay(200).duration(1000).springify()}
+                                source={images.highlight}
+                                className='self-center w-[250px] h-[250px] rounded-full mb-2'
+                            />
 
-                    {/* input section */}
-                    <Animated.View entering={FadeInDown.duration(1000).springify()} style={style.input_section}>
-                        <Text style={style.input_header}>Username or Email</Text>
-                        <View style={style.input_box}>
-                            <TextInput placeholder='example@gmail.com' placeholderTextColor={'#C9C9C9'} style={style.text_input_box} />
-                        </View>
-                    </Animated.View>
-                    <Animated.View entering={FadeInDown.duration(1000).springify()} style={style.input_section}>
-                        <Text style={style.input_header}>Password</Text>
-                        <View style={style.input_box}>
-                            <TextInput placeholder='password' placeholderTextColor={'#C9C9C9'} secureTextEntry={isPasswordVisbile} style={style.text_input_box} />
-                            <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisbile)}>
-                                <Image source={isPasswordVisbile ? icons.password_invisible : icons.password_visible} style={style.password_visibility_toggle} />
-                            </TouchableOpacity>
-                        </View>
-                    </Animated.View>
-
-                    {/* button section */}
-                    <View style={style.button_section}>
-                        <TouchableOpacity>
-                            <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} style={style.login_button}>
-                                <Text style={style.login_button_text}>Login</Text>
-                            </Animated.View>
-                        </TouchableOpacity>
-                        <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} style={style.register_text_section}>
-                            <Text style={style.warning_text}>Don't have an account? </Text>
-                            <TouchableOpacity onPress={() => navigation.push('/auth/signup')}>
-                                <Text style={style.register_text_link}>Signup</Text>
-                            </TouchableOpacity>
-                        </Animated.View>
-                    </View>
-
-                    {/* other option section  */}
-                    <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} style={style.other_option_section}>
-                        <Text style={style.warning_text}>___________________or___________________ </Text>
-                        <TouchableOpacity>
-                            <Animated.View entering={FadeInDown.delay(800).duration(1000).springify()} style={style.google_button}>
-                                <View style={style.google_button_content_section}>
-                                    <Image style={style.google_image} source={icons.google} />
-                                    <Text style={style.google_text}>Signup with Google</Text>
+                            {/* input section */}
+                            <Animated.View entering={FadeInDown.duration(1000).springify()} className='w-full'>
+                                <Text className='font-bold text-xl'>Username or Email</Text>
+                                <View className='flex-row justify-between items-center bg-white px-4 py-2 rounded-2xl w-full'>
+                                    <TextInput placeholder='example@gmail.com' placeholderTextColor={'#C9C9C9'} className='rounded-xl min-w-[80%]' />
                                 </View>
                             </Animated.View>
-                        </TouchableOpacity>
-                    </Animated.View>
-                </View>
-            </View>
-            {/* </ImageBackground > */}
-        </View >
+                            <Animated.View entering={FadeInDown.duration(1000).springify()} className='w-full'>
+                                <Text className='font-bold text-xl'>Password</Text>
+                                <View className='flex-row justify-between items-center bg-white px-4 py-2 rounded-2xl w-full'>
+                                    <TextInput placeholder='password' placeholderTextColor={'#C9C9C9'} secureTextEntry={isPasswordVisbile} className='rounded-xl min-w-[80%]' />
+                                    <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisbile)}>
+                                        <Image source={isPasswordVisbile ? icons.password_invisible : icons.password_visible} className='w-[25px] h-[25px]' />
+                                    </TouchableOpacity>
+                                </View>
+                            </Animated.View>
+
+                            {/* button section */}
+                            <View className='self-center mt-12 items-center gap-5'>
+                                <TouchableOpacity>
+                                    <Animated.View
+                                        entering={FadeInDown.delay(200).duration(1000).springify()}
+                                        className='bg-[#1A736A] border-white border-2 rounded-xl py-3 px-[69px]'
+                                    >
+                                        <Text className='font-bold color-white'>Login</Text>
+                                    </Animated.View>
+                                </TouchableOpacity>
+                                <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className='flex-row justify-center'>
+                                    <Text className='font-normal color-white text-[15px]'>Don't have an account? </Text>
+                                    <TouchableOpacity onPress={() => navigation.push('/auth/signup')}>
+                                        <Text className='font-medium text-[15px] color-[#48CAE4] underline'>Signup</Text>
+                                    </TouchableOpacity>
+                                </Animated.View>
+                            </View>
+
+                            {/* other option section  */}
+                            <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className='self-center items-center gap-4'>
+                                <Text className='font-normal color-white text-[15px]'>___________________or___________________ </Text>
+                                <TouchableOpacity>
+                                    <Animated.View
+                                        entering={FadeInDown.delay(800).duration(1000).springify()}
+                                        className='bg-white border-black border-1 rounded-sm py-3 px-[90px]'
+                                    >
+                                        <View className='flex-row gap-[10px]'>
+                                            <Image className='w-[20px] h-[20px]' source={icons.google} />
+                                            <Text className='font-bold color-black'>Signup with Google</Text>
+                                        </View>
+                                    </Animated.View>
+                                </TouchableOpacity>
+                            </Animated.View>
+                        </View>
+                    </View>
+                    {/* </ImageBackground > */}
+                </View >
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
--- frontend/app/auth/signup.tsx
+++ frontend/app/auth/signup.tsx
@@ -1,76 +1,97 @@
 
-import { View, Text, TextInput, ImageBackground, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import { images } from '@/constants/images';
 import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
 import { useRouter } from 'expo-router';
-import style from './authstyle';
 import { icons } from '@/constants/icons';
 
 const SingUp = () => {
     const [text, setText] = useState('');
+    const [isPasswordVisbile, setIsPasswordVisible] = useState(true);
     const navigation = useRouter();
 
     return (
-        <View style={{ flex: 1 }}>
-            <ImageBackground source={images.background} style={{ height: "100%", flex: 1 }}>
-                <View style={style.outerbox}>
-                    <View style={style.innerbox} >
-                        {/* logo section */}
-                        <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} source={images.highlight} style={style.logo_section} />
+        <KeyboardAvoidingView
+            className='flex-1'
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <View className='flex-1'>
+                    <Image source={images.background} className='h-full absolute' />
+                    <View className='flex-col h-full w-full justify-around '>
+                        <View className='flex-col items-start gap-5 px-11 py-5' >
+                            {/* logo section */}
+                            <Animated.Image
+                                entering={FadeInUp.delay(200).duration(1000).springify()}
+                                source={images.highlight}
+                                className='self-center w-[250px] h-[250px] rounded-full mb-2'
+                            />
 
-                        {/* input section */}
-                        <View style={style.input_section}>
-                            <Animated.Text entering={FadeInDown.duration(1000).springify()} style={style.input_header}>Username</Animated.Text>
-                            <Animated.View entering={FadeInDown.duration(1000).springify()} style={style.text_input_box}>
-                                <TextInput placeholder='John Doe' placeholderTextColor={'#C9C9C9'} />
+                            {/* input section */}
+                            <Animated.View entering={FadeInDown.duration(1000).springify()} className='w-full'>
+                                <Text className='font-bold text-xl'>Username</Text>
+                                <View className='flex-row justify-between items-center bg-white px-4 py-2 rounded-2xl w-full'>
+                                    <TextInput placeholder='John Doe' placeholderTextColor={'#C9C9C9'} className='rounded-xl min-w-[80%]' />
+                                </View>
                             </Animated.View>
-                        </View>
-                        <View style={style.input_section}>
-                            <Animated.Text entering={FadeInDown.duration(1000).springify()} style={style.input_header}>Email</Animated.Text>
-                            <Animated.View entering={FadeInDown.duration(1000).springify()} style={style.text_input_box}>
-                                <TextInput placeholder='example@gmail.com' placeholderTextColor={'#C9C9C9'} />
+                            <Animated.View entering={FadeInDown.duration(1000).springify()} className='w-full'>
+                                <Text className='font-bold text-xl'>Email</Text>
+                                <View className='flex-row justify-between items-center bg-white px-4 py-2 rounded-2xl w-full'>
+                                    <TextInput placeholder='example@gmail.com' placeholderTextColor={'#C9C9C9'} className='rounded-xl min-w-[80%]' />
+                                </View>
                             </Animated.View>
-                        </View>
-                        <View style={style.input_section}>
-                            <Animated.Text entering={FadeInDown.duration(1000).springify()} style={style.input_header}>Password</Animated.Text>
-                            <Animated.View entering={FadeInDown.duration(1000).springify()} style={style.text_input_box}>
-                                <TextInput placeholder='password' placeholderTextColor={'#C9C9C9'} secureTextEntry={true} />
+                            <Animated.View entering={FadeInDown.duration(1000).springify()} className='w-full'>
+                                <Text className='font-bold text-xl'>Password</Text>
+                                <View className='flex-row justify-between items-center bg-white px-4 py-2 rounded-2xl w-full'>
+                                    <TextInput placeholder='password' placeholderTextColor={'#C9C9C9'} secureTextEntry={isPasswordVisbile} className='rounded-xl min-w-[80%]' />
+                                    <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisbile)}>
+                                        <Image source={isPasswordVisbile ? icons.password_invisible : icons.password_visible} className='w-[25px] h-[25px]' />
+                                    </TouchableOpacity>
+                                </View>
                             </Animated.View>
-                        </View>
 
-                        {/* button section */}
-                        <View style={style.button_section}>
-                            <TouchableOpacity>
-                                <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} style={style.register_button}>
-                                    <Text style={style.register_button_text}>Register</Text>
+                            {/* button section */}
+                            <View className='self-center mt-12 items-center gap-5'>
+                                <TouchableOpacity>
+                                    <Animated.View
+                                        entering={FadeInDown.delay(400).duration(1000).springify()}
+                                        className='bg-[#1A736A] border-white border-2 rounded-xl py-3 px-[60px]'
+                                    >
+                                        <Text className='font-bold color-white'>Register</Text>
+                                    </Animated.View>
+                                </TouchableOpacity>
+                                <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className='flex-row justify-center'>
+                                    <Text className='font-normal color-white text-[15px]'>Already have an account? </Text>
+                                    <TouchableOpacity onPress={() => navigation.push('/auth/login')}>
+                                        <Text className='font-medium text-[15px] color-[#48CAE4] underline'>Login</Text>
+                                    </TouchableOpacity>
                                 </Animated.View>
-                            </TouchableOpacity>
-                            <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} style={style.register_text_section}>
-                                <Text style={style.warning_text}>Already have an account? </Text>
-                                <TouchableOpacity onPress={() => navigation.push('/auth/login')}>
-                                    <Text style={style.register_text_link}>Login</Text>
+                            </View>
+
+                            {/* other option section  */}
+                            <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className='self-center items-center gap-4'>
+                                <Text className='font-normal color-white text-[15px]'>___________________or___________________ </Text>
+                                <TouchableOpacity>
+                                    <Animated.View
+                                        entering={FadeInDown.delay(800).duration(1000).springify()}
+                                        className='bg-white border-black border-1 rounded-sm py-3 px-[90px]'
+                                    >
+                                        <View className='flex-row gap-[10px]'>
+                                            <Image className='w-[20px] h-[20px]' source={icons.google} />
+                                            <Text className='font-bold color-black'>Signup with Google</Text>
+                                        </View>
+                                    </Animated.View>
                                 </TouchableOpacity>
                             </Animated.View>
                         </View>
-
-                        {/* other option section  */}
-                        <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} style={style.other_option_section}>
-                            <Text style={style.warning_text}>___________________or___________________ </Text>
-                            <TouchableOpacity>
-                                <Animated.View entering={FadeInDown.delay(800).duration(1000).springify()} style={style.google_button}>
-                                    <View style={style.google_button_content_section}>
-
-                                        {/* <Image style={style.google_image} source={icons.google} /> */}
-                                        <Text style={style.google_text}>Signup with Google</Text>
-                                    </View>
-                                </Animated.View>
-                            </TouchableOpacity>
-                        </Animated.View>
                     </View>
-                </View>
-            </ImageBackground >
-        </View >
+                </View >
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 

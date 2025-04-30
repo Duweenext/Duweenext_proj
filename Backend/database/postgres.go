b4ecffb3ab5ffc166d9cacb090799cf080034b83
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-04-30 13:20:31 +0700

    [Update] backend

--- Backend/database/postgres.go
+++ Backend/database/postgres.go
@@ -10,37 +10,37 @@ import (
 )
 
 type postgresDatabase struct {
-  Db *gorm.DB
+	Db *gorm.DB
 }
 
 var (
-  once       sync.Once
-  dbInstance *postgresDatabase
+	once       sync.Once
+	dbInstance *postgresDatabase
 )
 
 func NewPostgresDatabase(conf *config.Config) Database {
-  once.Do(func() {
-    dsn := fmt.Sprintf(
-      "host=%s user=%s password=%s dbname=%s port=%d TimeZone=%s",
-      conf.Db.Host,
-      conf.Db.User,
-      conf.Db.Password,
-      conf.Db.DBName,
-      conf.Db.Port,
-      conf.Db.TimeZone,
-    )
-    
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-      panic("failed to connect database")
-    }
-    
-    dbInstance = &postgresDatabase{Db: db}
-  })
+	once.Do(func() {
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%d TimeZone=%s",
+			conf.Db.Host,
+			conf.Db.User,
+			conf.Db.Password,
+			conf.Db.DBName,
+			conf.Db.Port,
+			conf.Db.TimeZone,
+		)
 
-  return dbInstance
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
+		}
+
+		dbInstance = &postgresDatabase{Db: db}
+	})
+
+	return dbInstance
 }
 
 func (p *postgresDatabase) GetDb() *gorm.DB {
-  return dbInstance.Db
+	return p.Db
 }
--- Backend/go.mod
+++ Backend/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gofiber/storage/postgres/v3 v3.1.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect

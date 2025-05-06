049d72a8ba61ccfee2e4820a9f277efdde2206e7
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-05-07 05:12:51 +0700

    [Upgrade] index.tsx
    - add summary graph

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,7 +1,13 @@
+import BarChartExample from "@/components/BarChartExample";
+import LineChartExample from "@/components/LineChartExample";
+import { DropDown } from "@/components/menu";
+import PieChartExample from "@/components/PieChartExample";
 import TabBox from "@/components/TabBox";
 import { icons } from "@/constants/icons";
-import { useRouter } from "expo-router";
+import { router, useRouter } from "expo-router";
+import React, { useState } from "react";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Button, Menu } from "react-native-paper";
 
 const InfoSection = ({ time, harvest, waterchange }: { time: string, harvest: number, waterchange: number }) => {
   return (
@@ -20,15 +26,18 @@ const InfoSection = ({ time, harvest, waterchange }: { time: string, harvest: nu
     </>
   );
 }
-
 //router.push('/imageprocess')
 //router.push('/assistant')
 export default function Index() {
-  const router = useRouter();
+  const [selectedGraph, setSelectedGraph] = useState('Line graph');
+
+  const handleGraphSelect = (value: string) => {
+    setSelectedGraph(value);
+  };
 
   return (
     <ScrollView>
-      <View className="flex-1 justify-start items-start p-5 gap-6">
+      <View className="flex-1 justify-start items-start px-5 gap-6">
         <View className="flex-row justify-between items-center gap-3">
           <TabBox title="Sensor" icon={icons.sensor} onNav={() => router.push({
             pathname: '/(tabs)/sensor/[id]',
@@ -37,10 +46,50 @@ export default function Index() {
           <TabBox title="Check pond health" icon={icons.camera} onNav={() => null} />
           <TabBox title="Assistance" icon={icons.assistant} onNav={() => null} />
         </View>
-        <View className="bg-[#1A736A] w-full h-[0.75%] opacity-20" />
-        <InfoSection time="15 April 2025 (Saturday)" harvest={1} waterchange={2} />
-        <Text className="text-xl text-black font-bold text-start">Summary</Text>
+        <View className="bg-[#1A736A] w-full h-[0.25%] opacity-20" />
+        {/* <InfoSection time="15 April 2025 (Saturday)" harvest={1} waterchange={2} /> */}
+        <View className="flex-row justify-between items-center w-full">
+          <Text className="text-xl text-black font-bold text-start">Activity Overview</Text>
+          <DropDown
+            options={['Line graph', 'Bar graph', 'Pie graph']}
+            selected={selectedGraph}
+            onSelect={handleGraphSelect}
+            buttonText="Select Graph"
+          />
+        </View>
+        <Graphsection select={selectedGraph} />
       </View>
     </ScrollView>
   );
+}
+
+interface Graphtype {
+  select: string
+}
+
+const Graphsection = ({ select }: Graphtype) => {
+
+  switch (select) {
+    case 'Line graph': {
+      return (
+        <View className="flex w-auto">
+          <LineChartExample />
+        </View>
+      );
+    }
+    case 'Bar graph': {
+      return (
+        <View className="flex w-auto">
+          <BarChartExample />
+        </View>
+      );
+    }
+    case 'Pie graph': {
+      return (
+        <View className="flex w-auto">
+          <PieChartExample />
+        </View>
+      );
+    }
+  }
 }
